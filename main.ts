9d8fe42a89c4e2ea6f4ad0401313ccaf22306444
yamoyamoto <sf22588f@st.omu.ac.jp>
2022-10-22 13:42:58 +0900

    output node id & node name mapping as result

--- main.ts
+++ main.ts
@@ -10,6 +10,11 @@ type Deps = {
   to: String;
 };
 
+type DepsWithIds = {
+  from: Number;
+  to: Number;
+};
+
 function main() {
   const files = listFiles(dirPath);
   let depsList: Deps[] = [];
@@ -17,12 +22,75 @@ function main() {
     depsList = depsList.concat(parseFile(file));
   }
 
-  const outputFile = "result.edgelist";
-  const stream = fs.createWriteStream(outputFile);
-  for (const deps of depsList) {
-    stream.write(`${deps.from} ${deps.to}\n`);
+  const parsedDepsInfo = parseDepsList(depsList);
+
+  const edgelistFileName = "result.edgelist";
+  const edgelistStream = fs.createWriteStream(edgelistFileName);
+  for (const deps of parsedDepsInfo.depsListWithNodeIds) {
+    edgelistStream.write(`${deps.from} ${deps.to}\n`);
   }
-  stream.end();
+  edgelistStream.end();
+
+  const nodeIdMappingFileName = "node_ids.txt";
+  const nodeIdMappingStream = fs.createWriteStream(nodeIdMappingFileName);
+  for (const deps of parsedDepsInfo.nodeIdsAndNamesMapping) {
+    nodeIdMappingStream.write(`${deps.id} ${deps.name}\n`);
+  }
+  nodeIdMappingStream.end();
+}
+
+type Node = {
+  id: Number;
+  name: String;
+};
+
+function parseDepsList(depsList: Deps[]) {
+  const nodeIdsAndNamesMapping: Node[] = [];
+
+  let i = 0;
+  for (const deps of depsList) {
+    const from = nodeIdsAndNamesMapping.find((elem) => elem.name === deps.from);
+    if (from === undefined) {
+      nodeIdsAndNamesMapping.push({
+        id: i,
+        name: deps.from,
+      });
+      i++;
+    }
+
+    const to = nodeIdsAndNamesMapping.find((elem) => elem.name === deps.to);
+    if (to === undefined) {
+      nodeIdsAndNamesMapping.push({
+        id: i,
+        name: deps.to,
+      });
+      i++;
+    }
+  }
+
+  // edgelistをidに変換
+  const depsListWithNodeIds: DepsWithIds[] = [];
+  for (const deps of depsList) {
+    const fromNode = nodeIdsAndNamesMapping.find((elem) => elem.name == deps.from);
+    if (fromNode === undefined) {
+      throw new Error("nodeのidが見つかりませんでした");
+    }
+
+    const toNode = nodeIdsAndNamesMapping.find((elem) => elem.name == deps.to);
+    if (toNode === undefined) {
+      throw new Error("nodeのidが見つかりませんでした");
+    }
+
+    depsListWithNodeIds.push({
+      from: fromNode.id,
+      to: toNode.id,
+    });
+  }
+
+  return {
+    nodeIdsAndNamesMapping: nodeIdsAndNamesMapping,
+    depsListWithNodeIds: depsListWithNodeIds,
+  };
 }
 
 function extension(element: string) {
