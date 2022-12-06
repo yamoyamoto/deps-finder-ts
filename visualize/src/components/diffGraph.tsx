4fb9801513a7723895b20a4c42cd7f4e7942570b
yamoyamoto <sf22588f@st.omu.ac.jp>
2022-12-06 20:54:13 +0900

    add simple network using nivo

--- visualize/src/components/diffGraph.tsx
+++ visualize/src/components/diffGraph.tsx
@@ -1,5 +1,55 @@
 import * as react from "react";
+import { Network, ResponsiveNetwork, InputNode } from "@nivo/network";
+import * as network from "@nivo/network";
 
 export const App = () => {
-  return <div>hello!!!!</div>;
+  return (
+    <div>
+      <Network
+        data={{
+          nodes: [
+            {
+              id: "1",
+              height: 2,
+              size: 5,
+            },
+            {
+              id: "2",
+              height: 2,
+              size: 8,
+            },
+            {
+              id: "3",
+              height: 2,
+              size: 15,
+            },
+            {
+              id: "4",
+              height: 2,
+              size: 10,
+            },
+          ],
+          links: [
+            {
+              source: "1",
+              target: "2",
+            },
+            {
+              source: "2",
+              target: "3",
+            },
+            {
+              source: "2",
+              target: "4",
+            },
+          ],
+        }}
+        width={100}
+        height={100}
+        nodeSize={(node: any) => {
+          return node.size;
+        }}
+      ></Network>
+    </div>
+  );
 };
